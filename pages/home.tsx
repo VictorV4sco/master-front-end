0f9ca7b0a29f62e9b804a5031772991cfa94b201
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-05-16 19:28:40 -0300

    Home apperance

--- pages/home.tsx
+++ pages/home.tsx
@@ -9,13 +9,13 @@ export default function HomePage() {
     const [openPagamento, setOpenPagamento] = useState(false);
     const [openDespesa, setOpenDespesa] = useState(false);
     const router = useRouter();
-    
+
     useEffect(() => {
         const token = Cookies.get('token');
         if (!token) {
-            router.push('/login');  // Redireciona para login se não houver token
+            router.push('/login');
         }
-        }, [router]);
+    }, [router]);
 
     return (
         <div className={styles.container}>
@@ -24,24 +24,22 @@ export default function HomePage() {
 
             <div className={styles.main}>
 
-                <div>
+                <div className={styles.card}>
                     <h2 className={styles.subtitulo}>Pagamentos</h2>
                     <nav className={styles.navigation}>
                         <ul className={styles.ul}>
                             <li><Link className={styles.link} href="/insert-payment">Inserir</Link></li>
-                            <li><a className={styles.link} href="">Atualizar</a></li>
-                            <li><a className={styles.link} href="">Deletar</a></li>
-
+                            <li><a className={styles.link} href="#">Atualizar</a></li>
                             <li className={styles.dropdown}>
                                 <button className={styles.link} onClick={() => setOpenPagamento(!openPagamento)}>
                                     Checar pagamento
                                 </button>
                                 {openPagamento && (
                                     <ul className={styles.dropdownMenu}>
-                                    <li><a href="#">Número</a></li>
-                                    <li><a href="#">Dia</a></li>
-                                    <li><a href="#">Mês</a></li>
-                                    <li><a href="#">Ano</a></li>
+                                        <li><a href="#">Número</a></li>
+                                        <li><a href="#">Dia</a></li>
+                                        <li><a href="#">Mês</a></li>
+                                        <li><a href="#">Ano</a></li>
                                     </ul>
                                 )}
                             </li>
@@ -49,14 +47,12 @@ export default function HomePage() {
                     </nav>
                 </div>
 
-                <div>
+                <div className={styles.card}>
                     <h2 className={styles.subtitulo}>Despesas</h2>
                     <nav className={styles.navigation}>
                         <ul className={styles.ul}>
-                            <li><a className={styles.link} href="">Inserir</a></li>
-                            <li><a className={styles.link} href="">Atualizar</a></li>
-                            <li><a className={styles.link} href="">Deletar</a></li>
-                            
+                            <li><a className={styles.link} href="#">Inserir</a></li>
+                            <li><a className={styles.link} href="#">Atualizar</a></li>
                             <li className={styles.dropdown}>
                                 <button className={styles.link} onClick={() => setOpenDespesa(!openDespesa)}>
                                     Checar despesa
@@ -74,17 +70,16 @@ export default function HomePage() {
                     </nav>
                 </div>
 
-                <div>
+                <div className={styles.card}>
                     <h2 className={styles.subtitulo}>Academia</h2>
                     <nav className={styles.navigation}>
                         <ul className={styles.ul}>
-                            <li><a className={styles.link} href="">Checar ganhos</a></li>
+                            <li><a className={styles.link} href="#">Checar ganhos</a></li>
                         </ul>
                     </nav>
                 </div>
 
             </div>
-
         </div>
-    )
+    );
 }
